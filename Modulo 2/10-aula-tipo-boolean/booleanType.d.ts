6dc6988d785ee380019fd0d0e086fc3e15da627f
Raphael <raphaeldiass33@gmail.com>
2026-09-02 09:08:06 -0300

    boolean 2

--- Modulo 2/10-aula-tipo-boolean/booleanType.d.ts
+++ Modulo 2/10-aula-tipo-boolean/booleanType.d.ts
@@ -1,2 +1,10 @@
+/**
+ * arquivo: booleanType.ts
+ * descrição: arquivo responsável por ensinar conceitos básicos sobre 'Tipo Boolean'
+ * data: 02/09/2026
+ * author: Raphael Dias <@oraphadiass33>
+ * doc referência: https://www.typescriptlang.org/docs/handbook/basic-types.html#boolean
+ * Transpilação do arquivo: <Windows> CTRL + SHIFT + B -> tsc: build/watch
+ */
 export {};
 //# sourceMappingURL=booleanType.d.ts.map
--- Modulo 2/10-aula-tipo-boolean/booleanType.js
+++ Modulo 2/10-aula-tipo-boolean/booleanType.js
@@ -1,3 +1,24 @@
 "use strict";
+/**
+ * arquivo: booleanType.ts
+ * descrição: arquivo responsável por ensinar conceitos básicos sobre 'Tipo Boolean'
+ * data: 02/09/2026
+ * author: Raphael Dias <@oraphadiass33>
+ * doc referência: https://www.typescriptlang.org/docs/handbook/basic-types.html#boolean
+ * Transpilação do arquivo: <Windows> CTRL + SHIFT + B -> tsc: build/watch
+ */
 Object.defineProperty(exports, "__esModule", { value: true });
+// ==> Exemplo 01
+let tarefaConcluida = true;
+let tarefaPendente = false;
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+// ==> Exemplo 02
+let concluido = false;
+if (!concluido) {
+    console.log('Tarefa foi concluída com sucesso!');
+}
+else {
+    console.log('Tarefa Pendente!');
+}
 //# sourceMappingURL=booleanType.js.map
