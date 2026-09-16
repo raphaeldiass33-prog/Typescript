995b638823ddc7bcd64d3283b93b4fbc82023c09
Raphael <raphaeldiass33@gmail.com>
2026-09-16 12:19:33 -0300

    adicionado aula 22

--- Modulo 2/22-aula-tipo-any/anyTypes.ts
+++ Modulo 2/22-aula-tipo-any/anyTypes.ts
@@ -7,5 +7,24 @@
  */
 
 // ==> Exemplo 01: Tipo Any
+const a: any = 33;
+const b: any = ['Raphael'];
+
+const result = a + b;
+console.log(result);
+
 // ==> Exemplo 02: Quando o tipo 'any' é inferido implícitamente
+/*let frase;
+frase = 'Oi, pessoal! Tudo bem?';
+
+console.log(frase);*/
+
 // ==> Exemplo 03: Quando devemos usar o tipo any?!
+
+const formulario: { [campoFomulario: string]: any } = {
+  nome: 'Raphael',
+  sobrenome: 'Dias',
+  idade: 33,
+};
+
+console.log(formulario);
