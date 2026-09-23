79221792945b74199928d8fa5420985c84fe0047
Raphael <raphaeldiass33@gmail.com>
2026-09-23 10:14:19 -0300

    feat: adiciona aulas 26, 28, 30 e 32 do modulo 2

---
+++ Modulo 2/26 - aula-tipo-void/voidtype.js
@@ -0,0 +1,25 @@
+"use strict";
+/**
+ * arquivo: voidType.ts
+ * descrição: arquivo responsável por ensinar conceitos básicos sobre o tipo 'void'
+ * data: 27/08/2021
+ * author: Raphael Dias <Twitter: @oraphadias33>
+ * doc referência: https://www.typescriptlang.org/docs/handbook/basic-types.html#void
+ */
+// ==> Exemplo 01 - funções:
+function logError(errorMessage) {
+    console.log(errorMessage);
+    // return errorMessage;
+}
+logError('Required field - Name!');
+// ==> Exemplo 02 - funções:
+const logErrorExample2 = (errorMessage) => {
+    console.log(errorMessage);
+};
+logErrorExample2('Required field - Surname');
+// ==> Exemplo void: variáveis
+let variavelExemploVoid;
+// variavelExemploVoid = 1;
+variavelExemploVoid = null;
+variavelExemploVoid = undefined;
+console.log(variavelExemploVoid);
